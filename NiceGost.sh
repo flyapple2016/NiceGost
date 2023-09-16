26aaba62f081606cece50d47d5e50e071a57adcb
Kamren <23100346+flyapple2016@users.noreply.github.com>
2023-09-17 01:26:20 +0800

    Update NiceGost.sh

--- NiceGost.sh
+++ NiceGost.sh
@@ -2,9 +2,6 @@
 
 GO_VERSION="1.20.8"
 
-sudo apt-get update
-sudo apt-get install -y upx-ucl curl unzip gcc-aarch64-linux-gnu devscripts build-essential debhelper
-
 update_readme() {
   cat <<EOL > README.md
 Latest Version: $latest_version
@@ -70,6 +67,8 @@ current_version=$(curl -s "https://raw.githubusercontent.com/flyapple2016/NiceGo
 if [ "$latest_version" != "$current_version" ]; then
 
   echo "Remote Cloudflared version ($current_version) is different from latest version ($latest_version). Updating..."
+  sudo apt-get update
+  sudo apt-get install -y upx-ucl curl unzip gcc-aarch64-linux-gnu devscripts build-essential debhelper
   update_readme
   download_compile_upload
   build_deb_package
