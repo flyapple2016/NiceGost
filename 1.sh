6f89cfc1cf829d657551a5f1b7eff5201642dcc3
Kamren <23100346+flyapple2016@users.noreply.github.com>
2023-09-17 19:42:57 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "MY值是：$TELEGRAM_CHAT_ID"
-
+MESSAGE="NiceGost 测速."
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+curl -s -X POST $URL -d chat_id=$TELEGRAM_CHAT_ID -d text="$MESSAGE" > /dev/null
