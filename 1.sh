b024c3df35ad4c01ae159e76597d28907bb97daa
Kamren <23100346+flyapple2016@users.noreply.github.com>
2023-09-17 19:59:20 +0800

    Delete 1.sh

--- 1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-MESSAGE="NiceGost 测速."
-URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-curl -s -X POST $URL -d chat_id=$TELEGRAM_CHAT_ID -d text="$MESSAGE" > /dev/null
