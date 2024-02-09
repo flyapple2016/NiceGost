60f821e3e8d4861d1cb9ba760913033d8cef051c
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-02-09 14:36:27 +0800

    Update NiceGost.sh

--- NiceGost.sh
+++ NiceGost.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#GO_VERSION="1.21.6"
-
 update_readme() {
   cat <<EOL > README.md
 Latest Version: $latest_version
@@ -9,11 +7,6 @@ EOL
 }
 
 download_compile_upload() {
-#  wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
-#  sudo rm -rf /usr/local/go
-# sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-#  export PATH=$PATH:/usr/local/go/bin
-
   download_url="https://github.com/cloudflare/cloudflared/archive/$latest_version.zip"
   curl -LO "$download_url"
   unzip "$latest_version.zip"
