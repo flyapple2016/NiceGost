7a0b15a7c2fb9033edee251436e113c94b4dde97
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-03-13 13:12:54 +0800

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -19,4 +19,19 @@ done <<< "$content"
 echo "处理完成，结果已写入 $output_file 文件。"
 
 
-# https://addressesapi.090227.xyz/cmcc
+#!/bin/bash
+
+template="vless://3a9667dc-a684-1c1f-bdf0-c3098e8456ff@{{ip}}:{{port}}?encryption=none&security=xtls&flow=xtls-rprx-vision&sni=\$sni_host&type=ws&host=\$sni_host&path=/TerrariaSkeletron002?ed=2048#{{comment}}"
+
+addresses=$(curl -s https://addressesapi.090227.xyz/cmcc)
+ports=(443 2053 2083 2087 2096 8443)
+
+while IFS='#' read -r address comment; do
+    ip=$(echo "$address" | cut -d':' -f1)
+    port=${ports[$RANDOM % ${#ports[@]}]}
+    sni_host=$(echo "$comment" | cut -d'#' -f1)
+
+    new_template=$(echo "$template" | sed "s/{{ip}}/$ip/g; s/{{port}}/$port/g; s/{{comment}}/$comment/g")
+
+    echo "$new_template"
+done <<< "$addresses"
