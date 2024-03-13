22166eb0f19c0dceabd732322fa9d6811362ec80
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-03-13 11:35:26 +0800

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-curl https://sub.cfno1.eu.org/sub | base64 -d > text.txt
-sed '2,3d' text.txt > temp.txt
-sed 's/de44b044-8f90-4e18-b742-16591667ff96/3a9667dc-a684-1c1f-bdf0-c3098e8456ff/g; s/edgetunnel-free.pages.dev/zimbabwe-prefers-discounted-oldest.trycloudflare.com/g' temp.txt > text.txt
-base64 text.txt > encoded.txt
-rm temp.txt
+sni_host="zimbabwe-prefers-discounted-oldest.trycloudflare.com"
+
+template="vless://3a9667dc-a684-1c1f-bdf0-c3098e8456ff@{{ip_port}}?encryption=none&security=xtls&flow=xtls-rprx-vision&sni=$sni_host&type=ws&host=$sni_host&path=/TerrariaSkeletron002?ed=2048#{{comment}}"
+curl_output=$(curl https://sub.cfno1.eu.org/sub | base64 -d | sed '2,3d')
+output_file="modified_info.txt"
+> "$output_file"
+while IFS= read -r text; do
+    ip_port=$(echo "$text" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+')
+    comment=$(echo "$text" | grep -oE '#[^#]+$' | sed 's/#//')
+    result=$(echo "$template" | sed "s/{{ip_port}}/$ip_port/g; s/{{comment}}/$comment/g")
+    echo "$result" >> "$output_file"
+done <<< "$curl_output"
+echo "处理完成，结果已写入 $output_file 文件。"
