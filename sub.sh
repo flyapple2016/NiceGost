2003d367c5629cebc90e31467494ebe18d026798
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-03-13 19:10:40 +0800

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -3,7 +3,7 @@
 sni_host="zimbabwe-prefers-discounted-oldest.trycloudflare.com"
 template="vless://3a9667dc-a684-1c1f-bdf0-c3098e8456ff@{{ip}}:{{port}}?encryption=none&security=xtls&flow=xtls-rprx-vision&sni=$sni_host&type=ws&host=$sni_host&path=/TerrariaSkeletron002?ed=2048#{{comment}}"
 curl_output=$(curl https://sub.cfno1.eu.org/sub | base64 -d | sed '2,3d')
-output_file="encoded.txt"
+output_file="base64.txt"
 header=$(echo "$curl_output" | sed -n '1p')
 content=$(echo "$curl_output" | sed '1d')
 > "$output_file"
@@ -31,3 +31,5 @@ append_to_file() {
 }
 
 append_to_file
+
+base64 "$output_file" > encoded.txt
