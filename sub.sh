5bbbc31c93341f47fc009f76c4adc6559a3b6559
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-03-14 16:44:45 +0800

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+
 sni_host="zimbabwe-prefers-discounted-oldest.trycloudflare.com"
 template1="vless://3a9667dc-a684-1c1f-bdf0-c3098e8456ff@{{ip}}:{{port}}?encryption=none&security=xtls&flow=xtls-rprx-vision&sni=$sni_host&type=ws&host=$sni_host&path=/TerrariaSkeletron002?ed=2048#{{comment}}"
 template2="vless://3a9667dc-a684-1c1f-bdf0-c3098e8456ff@{{ip}}:{{port}}?encryption=none&security=xtls&flow=xtls-rprx-vision&sni=$sni_host&type=ws&host=$sni_host&path=/TerrariaSkeletron002?ed=2048#BR-{{comment}}"
@@ -10,8 +11,10 @@ curl_output=$(curl https://sub.cfno1.eu.org/sub | base64 -d | sed '2,3d')
 output_file="base64.txt"
 header=$(echo "$curl_output" | sed -n '1p')
 content=$(echo "$curl_output" | sed '1d')
+
 > "$output_file"
 echo "$header" >> "$output_file"
+
 while IFS= read -r text; do
     ip_port=$(echo "$text" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+')
     ip=$(echo "$ip_port" | cut -d ':' -f 1)
@@ -33,6 +36,7 @@ append_to_file() {
         new_template2=$(echo "$template2" | sed "s/{{ip}}/$ip/g; s/{{port}}/$port1/g; s/{{comment}}/$comment/g")
         echo "$new_template2" >> "$output_file"
     done <<< "$addresses"
+    
     while IFS='#' read -r address comment; do
         ip=$(echo "$address" | cut -d':' -f1)
         port2=${ports2[$RANDOM % ${#ports2[@]}]}
@@ -44,3 +48,5 @@ append_to_file() {
 }
 
 append_to_file
+
+base64 "$output_file" > encoded.txt
