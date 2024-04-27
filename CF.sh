4131d738529073e53fefc8b07cff8b58d69256c6
Kamren <23100346+flyapple2016@users.noreply.github.com>
2024-04-28 00:45:12 +0800

    Update CF.sh

--- CF.sh
+++ CF.sh
@@ -7,7 +7,7 @@ sleep 3
 
 asns=("13335" "209242")
 map_file="ISO2"
-rm -f IP4-AS13335-CIDR IP6-AS13335-CIDR IP4-AS209242-CIDR IP6-AS209242-CIDR IP4-AS209242-ISO
+rm -f IP4-AS13335-CIDR IP6-AS13335-CIDR IP4-AS209242-CIDR IP6-AS209242-CIDR IP4-AS209242-ISO IP4-AS13335-ISO
 
 for asn in "${asns[@]}"; do
     curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.100 Safari/537.36' "https://bgp.tools/table.txt" -s | grep "$asn" > "tmp-$asn.txt"
@@ -35,3 +35,11 @@ while read -r line; do
     chinese_name=$(grep "^${country_code}-" "$map_file" | cut -d '-' -f 2)
     echo $line $chinese_name >> "IP4-AS209242-ISO"
 done < IP4-AS209242-CIDR
+
+while read -r line; do
+    ip=$(echo $line | cut -d ' ' -f 1 | cut -d '/' -f 1)
+    result=$(mmdblookup --file GeoLite2-Country.mmdb --ip $ip country iso_code)
+    country_code=$(echo $result | awk -F '"' '{print $2}')
+    chinese_name=$(grep "^${country_code}-" "$map_file" | cut -d '-' -f 2)
+    echo $line $chinese_name >> "IP4-AS13335-ISO"
+done < IP4-AS13335-CIDR
